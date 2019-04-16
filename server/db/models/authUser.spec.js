07f52cbb3243e9061b34249a500c3ccf7f17d7a2
Lily Zdansky <lzdansky@mail.bradley.edu>
2019-04-16 15:12:32 -0400

    test spec for AuthUser Model added

---
+++ server/db/models/authUser.spec.js
@@ -0,0 +1,49 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const db = require('../index')
+const AuthUser = db.model('AuthUser')
+
+describe('AuthUser model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  let cody
+
+  beforeEach(async () => {
+    cody = await AuthUser.create({
+      firstname: 'cody',
+      email: 'cody@puppybook.com',
+      password: 'bones',
+      imageurl:
+        'https://upload.wikimedia.org/wikipedia/commons/a/a1/A_PUG_dog.jpg',
+      address: '129 E. Ruff Street, Brooklyn, NY'
+    })
+  })
+
+  describe('instanceMethods', () => {
+    describe('correctPassword', () => {
+      it('returns true if the password is correct', () => {
+        expect(cody.correctPassword('bones')).to.be.equal(true)
+      })
+
+      it('returns false if the password is incorrect', () => {
+        expect(cody.correctPassword('bonez')).to.be.equal(false)
+      })
+    }) // end describe('correctPassword')
+  }) // end describe('instanceMethods')
+  describe('Validations', () => {
+    it('requires `firstname`', async () => {
+      let jody = AuthUser.build()
+      try {
+        await jody.validate()
+        throw Error(
+          'validation was succesful but should have failed without `firstname`'
+        )
+      } catch (error) {
+        expect(error.message).to.contain('firstname cannot be null')
+      }
+    })
+  }) //end describe ('Validations')
+}) // end describe('AuthUser model')
