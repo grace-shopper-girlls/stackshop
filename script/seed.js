8af5a21410b3a1745c96fd1f42ae73f76dea0de5
Sandra <sandrajanineo@gmail.com>
2019-04-16 17:31:28 -0400

    wip issue #18

--- script/seed.js
+++ script/seed.js
@@ -1,44 +1,122 @@
-'use strict'
+const Fruit = require('../server/db/models/fruit')
+const db = require('../server/db/db')
+const {green, red} = require('chalk')
 
-const db = require('../server/db')
-const {User} = require('../server/db/models')
+const fruits = [
+  {
+    name: 'banana',
+    price: 1.0,
+    description: 'A yellow tropical fruit.',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Bananas.svg/2560px-Bananas.svg.png',
+    quantity: 10
+  },
+  {
+    name: 'raspberry',
+    price: 0.5,
+    description: 'A red berry fruit.',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/a/a6/Raspberries04.jpg',
+    quantity: 30
+  },
+  {
+    name: 'mango',
+    price: 5.0,
+    description: 'An orange tropical fruit.',
+    imageUrl:
+      'http://www.dreams.metroeve.com/wp-content/uploads/2017/10/www.dreams.metroeve.com-mango-dreams-meaning.jpg',
+    quantity: 6
+  }
+]
+
+const users = [
+  {
+    lastname: 'Zdansky',
+    firstname: 'Lily',
+    email: 'lzdansky@mail.bradley.edu',
+    password: 'abcGH1902',
+    imageurl: 'https://avatars3.githubusercontent.com/u/45503995?s=460&v=4',
+    address: '141 Quincy Street, Brooklyn, NY 11216',
+    salt: '',
+    googleId: ''
+  },
+  {
+    lastname: 'Strasser',
+    firstname: 'Lindsay',
+    email: 'lindsay.strasser@gmail.com',
+    password: 'p@ssword',
+    imageurl: 'https://avatars2.githubusercontent.com/u/22120272?s=460&v=4',
+    address: '3044 29th St. Astoria, NY 11102',
+    salt: '',
+    googleId: ''
+  },
+  {
+    lastname: 'Olascoaga',
+    firstname: 'Sandra',
+    email: 'sandrajanineo@gmail.com',
+    password: '123456789S!',
+    imageurl:
+      'https://i.pinimg.com/originals/12/e3/0d/12e30d479f91425271d4cc8cfa8fcc99.jpg',
+    address: '123 Mercedes Way Dr, Edgewood, NY 11717',
+    salt: '',
+    googleId: ''
+  },
+  {
+    lastname: 'Gretchen',
+    firstname: 'Opie',
+    email: 'gretchenopie@gmail.com',
+    password: '123456',
+    imageurl:
+      'https://upload.wikimedia.org/wikipedia/commons/5/5a/Michele_Pace_Del_Campidoglio_-_Still-Life_with_a_Female_Figure_-_WGA16798.jpg',
+    address: '349 Ocean Pkway, Brooklyn, NY 11218',
+    salt: '',
+    googleId: ''
+  }
+]
 
 async function seed() {
-  await db.sync({force: true})
-  console.log('db synced!')
+  try {
+    await db.sync({force: true})
+    console.log('db synced!')
 
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
+    await fruits.forEach(async function(fruit) {
+      await Fruit.create(fruit)
+    })
+  } catch (err) {
+    console.log(err)
+  }
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  // users.forEach(async function(user) {
+  //   await AuthUser.create(user)
+  // })
+
+  console.log(green('Seeding success!'))
+
+  // }
 }
 
-// We've separated the `seed` function from the `runSeed` function.
-// This way we can isolate the error handling and exit trapping.
-// The `seed` function is concerned only with modifying the database.
 async function runSeed() {
   console.log('seeding...')
   try {
     await seed()
   } catch (err) {
-    console.error(err)
+    console.log(err)
     process.exitCode = 1
   } finally {
     console.log('closing db connection')
-    await db.close()
+    // await db.close()
     console.log('db connection closed')
   }
 }
 
-// Execute the `seed` function, IF we ran this module directly (`node seed`).
-// `Async` functions always return a promise, so we can use `catch` to handle
-// any errors that might occur inside of `seed`.
 if (module === require.main) {
   runSeed()
 }
 
-// we export the seed function for testing purposes (see `./seed.spec.js`)
+// seed().catch(err => {
+//   console.error(red('Oh noes! Something went wrong!'))
+//   console.error(err)
+//   db.close()
+// })
+
 module.exports = seed
