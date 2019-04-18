157c19eb757f2795498b4e21355e249af4e10287
Grtchn <gretchenopie@gmail.com>
2019-04-18 15:19:25 -0400

    started work on addCartItem component

---
+++ client/components/cart-item-add.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addCartItemThunk } from '../store/cart';
+// ^^^ create that in the cart redux
+
+const AddCartItem = props => {
+  const { id, addCartItem } = props;
+  return (
+    <div>
+      <button
+        type="submit"
+        onClick={() => {
+          addCartItem(id);
+        }}
+      >
+        add to cart
+      </button>
+    </div>
+  );
+};
+
+const mapDispatch = dispatch => ({
+  addCartItem: id => dispatch(addCartItemThunk(id)),
+});
+
+export default connect(
+  null,
+  mapDispatch
+)(AddCartItem);
+
+
+<select>
+ <option value=“volvo”>Volvo</option>
+ <option value=“saab”>Saab</option>
+ <option value=“mercedes”>Mercedes</option>
+ <option value=“audi”>Audi</option>
+</select>
