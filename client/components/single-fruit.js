f3ec058eaded1bef05c43173bdf6605d6fa648b1
Grtchn <gretchenopie@gmail.com>
2019-04-19 15:28:17 -0400

    adding loading component to single fruit

--- client/components/single-fruit.js
+++ client/components/single-fruit.js
@@ -3,11 +3,12 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {fetchFruit} from '../store/fruits'
 import AddCartItem from './cart-item-add'
+import Loading from './loading'
 
 class SingleFruit extends React.Component {
   constructor(props) {
     super(props)
-    this.handleAddToCart = this.handleAddToCart.bind(this)
+    // this.handleAddToCart = this.handleAddToCart.bind(this)
   }
 
   componentDidMount() {
@@ -19,21 +20,19 @@ class SingleFruit extends React.Component {
     const fruit = this.props.fruit
     const loading = fruit.loading
 
-    if (loading) {
-      return <div>Loading...</div>
-    } else {
-      return (
-        <div>
-          <Link to="/"> Back to All Fruit </Link>
-          <img className="zoomFruitImg" src={fruit.imageUrl} />
-          <h1 className="fruitName"> {fruit.name} </h1>
-          <h3> Description: {fruit.description} </h3>
-          <h2> Price: $ {fruit.price} </h2>
-          <h2> Quantity Available: {fruit.quantity} </h2>
-          <AddCartItem fruit={fruit} />
-        </div>
-      )
-    }
+    return loading ? (
+      <Loading />
+    ) : (
+      <div>
+        <Link to="/"> Back to All Fruit </Link>
+        <img className="zoomFruitImg" src={fruit.imageUrl} />
+        <h1 className="fruitName"> {fruit.name} </h1>
+        <h3> Description: {fruit.description} </h3>
+        <h2> Price: $ {fruit.price} </h2>
+        <h2> Quantity Available: {fruit.quantity} </h2>
+        <AddCartItem fruit={fruit} />
+      </div>
+    )
   }
 }
 
