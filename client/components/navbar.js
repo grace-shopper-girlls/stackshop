4c23205a720eb929e672963b52735f325eb306db
Sandra <sandrajanineo@gmail.com>
2019-04-20 13:03:27 -0400

    added links to navbar and css to make navbar static at the top of every page

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,21 +6,37 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
     <nav>
+      <h1>Welcome to Cute Fruits!</h1>
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
+          <Link to="/">All Fruits</Link>
           <Link to="/home">Home</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
+          <Link to="/cart">
+            View cart
+            <img
+              className="cartImage"
+              src="http://simpleicon.com/wp-content/uploads/Shopping-Cart-11.png"
+            />
+          </Link>
         </div>
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
+          <Link to="/">All Fruits</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
+          <Link to="/cart">
+            View cart
+            <img
+              className="cartImage"
+              src="http://simpleicon.com/wp-content/uploads/Shopping-Cart-11.png"
+            />
+          </Link>
         </div>
       )}
     </nav>
