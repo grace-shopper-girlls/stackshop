376ff063638491a3a7eb3688b2cfe8bc7efa3505
Sandra <sandrajanineo@gmail.com>
2019-04-23 11:57:05 -0400

    added PUT route for orders submitted

--- server/api/orders.js
+++ server/api/orders.js
@@ -18,3 +18,18 @@ router.get('/:userId', async (req, res, next) => {
     next(err)
   }
 })
+
+//for when logged-in user submits an order
+router.put('/', async (req, res, next) => {
+  let body = req.body
+  try {
+    await Order.update({
+      where: {
+        id: body.id
+      },
+      orderSubmitted: true
+    })
+  } catch (error) {
+    next(error)
+  }
+})
